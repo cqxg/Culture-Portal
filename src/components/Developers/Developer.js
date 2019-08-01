e8f410796751ce41345c95fd73e946513ca5eac7
Tatyana Shulha <tsulga6@gmail.com>
2019-08-01 15:16:17 +0300

    refactor: change styles for developer block

--- src/components/Developers/Developer.js
+++ src/components/Developers/Developer.js
@@ -9,12 +9,10 @@ const Developer = () => (
       <Col lg={2} md={6} xs={12}>
         <div className={developerStyles.developer_block}>
           <div className={developerStyles.developer_photo} style={{ backgroundImage: "url('https://i.ytimg.com/vi/XjoFxyjSuVI/mqdefault.jpg')"}}>
-
           </div>
-          {/* <img src="https://i.ytimg.com/vi/XjoFxyjSuVI/mqdefault.jpg"/> */}
-          <div>
-            <p className={developerStyles.developer_name}>BLA BLA</p>
-            <p className={developerStyles.developer_nickname}>blabla</p>
+          <div className={developerStyles.developer_information}>
+            <p className={developerStyles.developer_name}>Name Surname</p>
+            <p className={developerStyles.developer_nickname}>github_nick</p>
             <a target="_blank" href="https://github.com">
              <img src="https://anart.ru/img/2014/04/octocat.png" />
             </a>
