3e0575363f9af8e1f4a389ec6931588f5bf13056
Tatyana Shulha <tsulga6@gmail.com>
2019-08-01 18:44:22 +0300

    fix: add data array for developer cards

--- src/components/Developers/Developer.js
+++ src/components/Developers/Developer.js
@@ -3,24 +3,71 @@ import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import developerStyles from './developer.module.less';
 
-const Developer = () => (
-  <Container className={developerStyles.developers_content}>
-    <Row>
-      <Col lg={2} md={6} xs={12}>
-        <div className={developerStyles.developer_block}>
-          <div className={developerStyles.developer_photo} style={{ backgroundImage: "url('https://i.ytimg.com/vi/XjoFxyjSuVI/mqdefault.jpg')"}}>
-          </div>
-          <div className={developerStyles.developer_information}>
-            <p className={developerStyles.developer_name}>Name Surname</p>
-            <p className={developerStyles.developer_nickname}>github_nick</p>
-            <a target="_blank" href="https://github.com">
-             <img src="https://anart.ru/img/2014/04/octocat.png" />
-            </a>
-          </div>
-        </div>
-      </Col>
-    </Row>
-  </Container>
+const teamData = [
+  {
+    url: "https://www.meme-arsenal.com/memes/4808db7e65492b7233abf5fb753ab0a4.jpg",
+    name: "Name Surname",
+    git: "git_nock",
+    href: "https://github.com"
+  },
+  {
+    url: "https://www.meme-arsenal.com/memes/4808db7e65492b7233abf5fb753ab0a4.jpg",
+    name: "Name Surname",
+    git: "git_nock",
+    href: "https://github.com"
+  },
+  {
+    url: "https://www.meme-arsenal.com/memes/4808db7e65492b7233abf5fb753ab0a4.jpg",
+    name: "Name Surname",
+    git: "git_nock",
+    href: "https://github.com"
+  },
+  {
+    url: "https://www.meme-arsenal.com/memes/4808db7e65492b7233abf5fb753ab0a4.jpg",
+    name: "Name Surname",
+    git: "git_nock",
+    href: "https://github.com"
+  },
+  {
+    url: "https://www.meme-arsenal.com/memes/4808db7e65492b7233abf5fb753ab0a4.jpg",
+    name: "Name Surname",
+    git: "git_nock",
+    href: "https://github.com"
+  },
+  {
+    url: "https://www.meme-arsenal.com/memes/4808db7e65492b7233abf5fb753ab0a4.jpg",
+    name: "Name Surname",
+    git: "git_nock",
+    href: "https://github.com"
+  }
+];
+
+const Developer = (props) => ( 
+  <Col lg={2} md={6} xs={12}>
+    <div className={developerStyles.developer_block}>
+      <img className={developerStyles.developer_photo} src={props.url}/>
+      <div className={developerStyles.developer_information}>
+        <p className={developerStyles.developer_name}>{props.name}</p>
+        <p className={developerStyles.developer_nickname}>{props.git}</p>
+        <a target="_blank" href={props.href}>
+          <img className={developerStyles.git_photo} src="https://anart.ru/img/2014/04/octocat.png" />
+        </a>
+      </div>
+    </div>
+  </Col> 
 )
 
-export default Developer;
+const DevelopersTeam = (props) => {
+  const developers = teamData.map((developer, i) => {
+    return <Developer key={i} url={developer.url} name={developer.name} git={developer.git} href={developer.href} />
+  });
+  
+  return <Container className={developerStyles.developers_content}>
+            <Row>
+              {developers}
+            </Row>
+          </Container>
+              
+}
+
+export default DevelopersTeam;
