257bb2a9e73f6935523cdb42a725fe7b5f864e5b
Eri <stayagolubcov@gmail.com>
2019-08-01 04:00:48 +0300

    feat: add dynamic list of directors  accoding to locale

--- src/pages/directors.js
+++ src/pages/directors.js
@@ -1,29 +1,74 @@
 import React from "react";
-import { FormattedMessage, Link, injectIntl } from "gatsby-plugin-intl";
+import { Link, injectIntl, IntlContextConsumer } from "gatsby-plugin-intl";
+import { StaticQuery, graphql } from 'gatsby';
+
 import Layout from "../components/Layouts/Layout";
 import SEO from "../components/seo";
+import '../mainStyles/directors.less';
 
-const Directors = ({ intl }) => (
-  <Layout>
-    <SEO
-      lang={intl.locale}
-      title={intl.formatMessage({ id: "directors.titleDirector" })}
-    />
-    <h1>
-      <FormattedMessage id="directors.helloDirectors" />
-    </h1>
-    <p>
-      <FormattedMessage id="directors.welcomeDirectors" />
-    </p>
-    <Link to="/">
-      <FormattedMessage id="backHomePage" />
-    </Link>
-    <p>
-    <Link to="/director-page">
-      <FormattedMessage id="toDirector" />
-    </Link>
-    </p>
-  </Layout>
-)
+
+const Directors = ({ intl }) => {
+  const getNames = (data, lang) => {
+    const namesArray = [];
+    data.allDirectorsJson.edges.forEach((element, index) => {
+      console.log(element.node.id);
+      namesArray.push(
+      <Link 
+      key={index+1} 
+      to='/director-page/'
+      state={{id: element.node.id}}
+      >
+      {element.node[lang].name}
+      </Link>)
+    });
+    return namesArray;
+  };
+  return (
+    <Layout>
+      <SEO
+        lang={intl.locale}
+        title={intl.formatMessage({ id: "directors.titleDirector" })}
+      />
+      <IntlContextConsumer>
+        {({ languages, language: currentLocale }) => {
+          const lang = currentLocale;
+          console.log(lang);          
+          return <StaticQuery
+            query={graphql`
+        query NamesQuery {
+          allDirectorsJson {
+            edges {
+              node {
+                be {
+                  name
+                }
+                en {
+                  name
+                }
+                ru {
+                  name
+                }
+                id
+              }
+            }
+          }
+        }
+        `}
+            render={data => (
+              <>
+                <div className="directors">
+                  {getNames(data, lang)}
+                </div>
+              </>
+            )}
+          >
+
+          </StaticQuery>
+        }
+        }
+      </IntlContextConsumer>
+    </Layout>
+  )
+}
 
 export default injectIntl(Directors);
