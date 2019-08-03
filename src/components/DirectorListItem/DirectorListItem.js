f0a8c7a8f16477e4f73c38260bc2756856fd083d
Eri <stayagolubcov@gmail.com>
2019-08-03 20:32:00 +0300

    feat: add search logic refactor: extract  directors list item into separate component

---
+++ src/components/DirectorListItem/DirectorListItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link, injectIntl } from "gatsby-plugin-intl";
+import { Row, Col } from 'reactstrap';
+import PropTypes from 'prop-types';
+import directorsStyles from '../../mainStyles/directors.module.less';
+
+
+const DirectorListItem = ({ directorData, locale }) => {
+    return (
+        <Row>
+            <Col lg={12} className={directorsStyles.mainTitle}>
+                <Link
+                    to='/director-page'
+                    state={{ directorData: directorData }}
+                >
+                    {`${directorData[locale].name}, ${directorData[locale].location}`}
+                </Link>
+            </Col>
+        </Row>
+    )
+};
+
+export default DirectorListItem;
+
+
+DirectorListItem.propTypes = {
+    directorData: PropTypes.instanceOf(Object).isRequired
+};
