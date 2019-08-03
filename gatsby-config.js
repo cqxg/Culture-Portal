c3172b108b8f154f723d693d5d4963436e44a5fd
Eri <stayagolubcov@gmail.com>
2019-08-04 00:42:12 +0300

    git: merge master into eri

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,9 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-intl',
       options: {
-        // language JSON resource path
         path: `${__dirname}/src/intl`,
-        // supported language
         languages: ['en', 'ru', 'be'],
-        // language file path
         defaultLanguage: 'en',
-        // option to redirect to `/ko` when connecting `/`
         redirect: true,
       },
     },
---
+++ src/components/Image/Image.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+
+function renderImage(file) {
+    return <Img fluid={file.node.childImageSharp.fluid} alt={file.node.name} />;
+}
+
+const Image = function(props) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          images: allFile(
+            filter: { extension: { regex: "/jpeg|jpg|png|gif/" } }
+          ) {
+            edges {
+              node {
+                name
+                extension
+                relativePath
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      `}
+
+      render={({ images }) =>
+        renderImage(images.edges.find(image => {
+            return image.node.relativePath === props.src;
+        }))
+      }
+    />
+  )
+}
+
+export default Image;
