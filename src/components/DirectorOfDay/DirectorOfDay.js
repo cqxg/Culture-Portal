35c5f1fc65c0e3d385c489dbda4c9e7eafd17b9a
Eri <stayagolubcov@gmail.com>
2019-08-04 00:34:49 +0300

    feat: add functionality for director of the day fix: change fields in DirectorOfTheDay to get data correctly

--- src/components/DirectorOfDay/DirectorOfDay.js
+++ src/components/DirectorOfDay/DirectorOfDay.js
@@ -1,51 +1,44 @@
 import React from "react";
-import { FormattedMessage, Link } from "gatsby-plugin-intl";
+import { FormattedMessage, Link, injectIntl } from "gatsby-plugin-intl";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col, Button } from "reactstrap";
-
+import { Row, Col, Button } from "reactstrap";
 import directorOfDayStyles from "./directorOfDay.module.less";
 
-// const director = {
-//   name: "Evklid Seventeen",
-//   birth: "30 january",
-//   death: "23 june",
-//   img: "ddas",
-//   description:
-//     "loves write, drink, and musicloves write, drink, and musicloves write, drink, and musicloves write, drink, and musicloves write, drink, and musicloves write, drink, and musicloves write, drink, and musicloves write, drink, and musicloves write, drink, and musicloves write, drink, and musicloves write, drink, and musicloves write, drink, and musicloves write, drink, and music",
-//   link: "thisway",
-//   videoId: 'u1QF-LWNUdY'
-// }
+const DirectorOfDay = ({ intl, director, locale }) => {
+    console.log( intl, director, locale);
+    return (
+        <Col className={directorOfDayStyles.directorOfDay_container}>
+            <Row className={directorOfDayStyles.directorOfDay_title_container}>
+                <h2>
+                    <FormattedMessage id="index.dirOfTheDay" />
+                </h2>
+            </Row>
 
-const DirectorOfDay = ({ director }) => (
-  <Col className={directorOfDayStyles.directorOfDay_container}>
-    <Row className={directorOfDayStyles.directorOfDay_title_container}>
-      <h2>Director of the day</h2>
-    </Row>
+            <Row className={directorOfDayStyles.directorOfDay_content_container}>
+                <Col md={6} xs={{ size: 12, order: 1 }}>
+                    <h3>{director[intl.locale].name}</h3>
+                    <p>{director[intl.locale].yearsOfLife}</p>
+                </Col>
 
-    <Row className={directorOfDayStyles.directorOfDay_content_container}>
-      <Col md={7} xs={{ size: 12, order: 1 }}>
-        <h3>{director.name}</h3>
-        <p>{`${director.birth} - ${
-          director.death ? director.death : "Alive"
-        }`}</p>
-      </Col>
+                <Col md={6} xs={{ size: 12, order: 3 }}>
+                    <h4>{director[intl.locale].bio}</h4>
+                    <Link to="/" className={directorOfDayStyles.contentButton}>
+                        <Button type="button" className="btn btn-secondary btn-block">
+                            <FormattedMessage id="index.more" />
+                        </Button>
+                    </Link>
+                </Col>
 
-      <Col md={7} xs={{ size: 12, order: 3 }}>
-        <h4>{director.description}</h4>
-        <Link to="/" className={directorOfDayStyles.contentButton}>
-          <Button type="button" class="btn btn-secondary btn-block">More about</Button>
-        </Link>
-      </Col>
-
-      <Col md={5} xs={{ size: 12, order: 2 }}>
-        <div
-          className={directorOfDayStyles.directorOfDay_container_content_img_resize}
-        >
-          <img src={director.img} alt="director_avatar" />
-        </div>
-      </Col>
-    </Row>
-  </Col>
-)
+                <Col md={5} xs={{ size: 12, order: 2 }}>
+                    <div
+                        className={directorOfDayStyles.directorOfDay_container_content_img_resize}
+                    >
+                        <img src={director.img} alt="director_avatar" />
+                    </div>
+                </Col>
+            </Row>
+        </Col>
+    )
+};
 
 export default DirectorOfDay;
