8aaca7d934a89c98e83ea12beeb0a5f39cb2163b
Eri <stayagolubcov@gmail.com>
2019-08-04 23:42:08 +0300

    fix: change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,13 +27,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
+        name: 'Belarusian Directors Portal',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/directors-favicon.jpg', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-less',
