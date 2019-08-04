b89419b613bca97149ecf4c2b31ae52b214fbea7
Eri <stayagolubcov@gmail.com>
2019-08-04 22:40:19 +0300

    refactor: delete console logs, fix indentations

--- src/pages/directors.js
+++ src/pages/directors.js
@@ -10,59 +10,56 @@ import directorsInfo from '../queries/allDirectorsJson';
 import SearchBar from '../components/Search/SearchBar';
 
 const Directors = ({ intl }) => {
-    const directorsInfoJson = directorsInfo().allDirectorsJson.edges.map(node => node.node);
-    const handleSort = (a, b) => {
-        const compare1 = a[intl.locale].name.toLowerCase();
-        const compare2 = b[intl.locale].name.toLowerCase();
+  const directorsInfoJson = directorsInfo().allDirectorsJson.edges.map(node => node.node);
+  const handleSort = (a, b) => {
+    const compare1 = a[intl.locale].name.toLowerCase();
+    const compare2 = b[intl.locale].name.toLowerCase();
 
-        if(compare1 < compare2) { return -1; }
-        if(compare1 > compare2) { return 1; }
-        return 0
-    };
-    const sortedDirectors = directorsInfoJson.sort(handleSort);
-    const [directorsAfterSearch, setDirectorsAfterSearch] = useState(sortedDirectors);
-    const searchFunc = (lookFor) => {
-        console.log(lookFor);
-        if (lookFor.value) {
-            const searchBoth = (target) => {
-                console.log(target);
+    if (compare1 < compare2) { return -1; }
+    if (compare1 > compare2) { return 1; }
+    return 0
+  };
+  const sortedDirectors = directorsInfoJson.sort(handleSort);
+  const [directorsAfterSearch, setDirectorsAfterSearch] = useState(sortedDirectors);
+  const searchFunc = (lookFor) => {
+    if (lookFor.value) {
+      const searchBoth = (target) => {
+        return (
+          target[intl.locale].name.toLowerCase().includes(lookFor.value.toLowerCase())
+          || target[intl.locale].location.toLowerCase().includes(lookFor.value.toLowerCase()))
+      };
+      const result = directorsInfoJson.filter(dir => searchBoth(dir));
+      const sortedResult = result.sort(handleSort);
+      setDirectorsAfterSearch(sortedResult);
+    } else {
+      setDirectorsAfterSearch(directorsInfoJson);
+    }
+  };
 
-                return (
-                    target[intl.locale].name.toLowerCase().includes(lookFor.value.toLowerCase())
-                    || target[intl.locale].location.toLowerCase().includes(lookFor.value.toLowerCase()))
-            };
-            const result = directorsInfoJson.filter(dir => searchBoth(dir));
-            const sortedResult = result.sort(handleSort);
-            setDirectorsAfterSearch(sortedResult);
-        } else {
-            setDirectorsAfterSearch(directorsInfoJson);
-        }
-    };
-
-    return (<Layout>
-        <Container className={directorsStyles.directors_container}>
-            <SEO
-                lang={intl.locale}
-                title={intl.formatMessage({ id: "directors.titleDirector" })}
-            />
-            <Row>
-                <Col lg={12} className={directorsStyles.mainTitle}>
-                    <h1>
-                        <FormattedMessage id="directors.helloDirectors" />
-                    </h1>
-                </Col>
-            </Row>
-            <Row>
-                <SearchBar sortFunc={searchFunc} />
-            </Row>
-            {directorsAfterSearch.map((director, index) => <DirectorListItem
-                directorData={director}
-                locale={intl.locale}
-                key={index}
-            ></DirectorListItem>)}
-        </Container>
-    </Layout>
-    )
+  return (<Layout>
+    <Container className={directorsStyles.directors_container}>
+      <SEO
+        lang={intl.locale}
+        title={intl.formatMessage({ id: "directors.titleDirector" })}
+      />
+      <Row>
+        <Col lg={12} className={directorsStyles.mainTitle}>
+          <h1>
+            <FormattedMessage id="directors.helloDirectors" />
+          </h1>
+        </Col>
+      </Row>
+      <Row>
+        <SearchBar sortFunc={searchFunc} />
+      </Row>
+      {directorsAfterSearch.map((director, index) => <DirectorListItem
+        directorData={director}
+        locale={intl.locale}
+        key={index}
+      ></DirectorListItem>)}
+    </Container>
+  </Layout>
+  )
 }
 
 export default injectIntl(Directors);
