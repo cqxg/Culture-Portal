1cc4203ccfa52462ac1367a16c5bd35ec2fa2a34
Eri <stayagolubcov@gmail.com>
2019-08-04 12:11:29 +0300

    fix: try to deploy site

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,7 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
-import RBCarousel from "react-bootstrap-carousel";
+// import RBCarousel from "react-bootstrap-carousel";
 
 import carouselStyles from './carousel.module.less';
 
@@ -9,14 +9,14 @@ const Carousel = (props) => {
   const slides = props.images.map((image, i) => <img key={i} src={image.image} />);
 
   return (
-    <RBCarousel className={carouselStyles.carousel_block}
-      animation={true}
-      autoplay={true}
-      slideshowSpeed={2000}
-      version={4}
-    >
-      {slides}
-    </RBCarousel>
+    // <RBCarousel className={carouselStyles.carousel_block}
+    //   animation={true}
+    //   autoplay={true}
+    //   slideshowSpeed={2000}
+    //   version={4}
+    // >
+    //   {slides}
+    // </RBCarousel>
   );
 }
 
