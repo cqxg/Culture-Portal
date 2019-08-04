6eba18a1b4a95ede791bec526ebda93bd44fd2b1
tanyshulha <tsulga6@gmail.com>
2019-08-04 14:05:45 +0300

    fix: replace plugin bootstrap carousel by native one

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,23 +1,22 @@
 import React from "react";
+import { Carousel as BootstrapCarousel } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
-// import RBCarousel from "react-bootstrap-carousel";
+import Image from "../Image/Image";
 
 import carouselStyles from './carousel.module.less';
 
 const Carousel = (props) => {
-  const slides = props.images.map((image, i) => <img key={i} src={image.image} />);
+  const slides = props.images.map((image, i) => {
+    return <BootstrapCarousel.Item>
+      <Image src={image.image} />
+    </BootstrapCarousel.Item>
+  });
 
-//   return (
-    // <RBCarousel className={carouselStyles.carousel_block}
-    //   animation={true}
-    //   autoplay={true}
-    //   slideshowSpeed={2000}
-    //   version={4}
-    // >
-    //   {slides}
-    // </RBCarousel>
-//   );
+  return (
+    <BootstrapCarousel>
+      {slides}
+    </BootstrapCarousel>
+  );
 }
 
 export default Carousel;
